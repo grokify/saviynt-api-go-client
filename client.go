e45b1df7d6d1b5477a411421421ee5f5bf8e65ec
priyanshu.solanki <priyanshu.solanki@Priyanshus-MacBook-Pro.local>
2025-02-25 11:35:10 +0530

    test

--- client.go
+++ client.go
@@ -56,7 +56,7 @@ type Client struct {
 	UsersAPI                      *users.UsersAPIService
 	usersClient                   *users.APIClient
 	EndPointAPI                   *endpoint.EndPointAPIService
-	endpointClient                *endpoint.APIClient
+	endpointclient                *endpoint.APIClient
 }
 
 func NewClient(ctx context.Context, serverURL string, httpClient *http.Client) *Client {
